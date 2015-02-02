dbef692e9c0b15774fdd0d07e8f13d62d8a291f5
Jose Zamudio <zamudio@outlook.com>
2015-02-01 23:13:18 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session      = require('express-session');
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+mongoose.connect(process.env.MONGO_URL);
+require('./config/passport')(passport);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'toddsuckstoomuch', saveUninitialized: true, resave: false }));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+var passportRoutes = require('./app/routes.js');
+passportRoutes(app, passport);
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'facebookAuth' : {
+    'clientID'     : '394160284088632',
+    'clientSecret'  : 'bbfcd7e8040fb4af3fd5517a63edf6e3',
+    'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+  }
+}
